d72225254948966164f016530756eefe9a987113
ChillerDragon <ChillerDragon@gmail.com>
2022-09-25 15:31:06 +0200

    Support string interpolation

--- bash-to-rust.sh
+++ bash-to-rust.sh
@@ -70,10 +70,76 @@ function scope_is_str_fn() {
 	return 1
 }
 
+function _recurse_vars_in_fmt() {
+	# do not call this directly
+	# it is just a recursive helper
+	#
+	# give a unquoted format string and the filling vars
+	# as two parameters
+	# it will search if there is one more bash var
+	# in the format string
+	# if so it will call it self
+	# with the new format string and parameter list
+	#
+	# if no further variables found it will return
+	# the full rust format statement
+	#
+	# EXAMPLE
+	#
+	#   _recurse_vars_in_fmt "hello {} $var" "world"
+	#
+	#    -> "format!("hello {} {}", world, var)"
+
+	local fmt_str="$1"
+	local var_list="$2"
+	if [[ "$fmt_str" =~ (.*)\$([a-zA-Z_][a-zA-Z0-9_]*)(.*) ]]
+	then
+		local str_left
+		local str_right
+		local var
+		var="${BASH_REMATCH[2]}"
+		str_left="${BASH_REMATCH[1]}"
+		str_right="${BASH_REMATCH[3]}"
+
+		# insert format placeholder
+		printf -v fmt_str '%s{}%s' "$str_left" "$str_right"
+		# append var to list
+		var_list="$var, $var_list"
+
+		_recurse_vars_in_fmt "$fmt_str" "$var_list"
+	else
+		printf 'format!(%s", %s)' "$fmt_str" "$var_list"
+	fi
+}
+function interpolate_to_fmt() {
+	# given a interpolated and quoted
+	# bash string
+	# it returns a rust format! macro
+	#
+	# if it could not find a proper quoted string
+	# it prints nothing
+	local str="$1"
+	if [[ "$str" =~ ^\"(.*)\$([a-zA-Z_][a-zA-Z0-9_]*)(.*)\"$ ]]
+	then
+		local str_left
+		local str_right
+		local fmt_str
+		local var
+		var="${BASH_REMATCH[2]}"
+		str_left="${BASH_REMATCH[1]}"
+		str_right="${BASH_REMATCH[3]}"
+
+		printf -v fmt_str '"%s{}%s' "$str_left" "$str_right"
+
+		_recurse_vars_in_fmt "$fmt_str" "$var"
+	else
+		echo ""
+	fi
+}
 function bash_value_to_rust() {
 	local val="$1"
 	# quoted var
-	if [[ "$val" =~ ^\"\$(.*)\" ]]
+	if [[ "$val" =~ ^\"\$([a-zA-Z_][a-zA-Z0-9_])\"$ ]]
 	then
 		val="${BASH_REMATCH[1]}"
 	# unquoted var
@@ -87,7 +153,11 @@ function bash_value_to_rust() {
 	# quoted string
 	elif [[ "$val" =~ ^\".*\" ]]
 	then
-		val="String::from($val)"
+		local owned_string="String::from($val)"
+		val="$(interpolate_to_fmt "$val")"
+		# no interpolation found
+		# fallback to String
+		[[ "$val" == "" ]] && val="$owned_string"
 	# unquoted string
 	elif [[ "$val" =~ ^([^ ]+) ]]
 	then
@@ -101,7 +171,9 @@ function bash_value_to_rust() {
 function bash_value_to_str_slice() {
 	local val="$1"
 	# quoted var
-	if [[ "$val" =~ ^\"\$(.*)\" ]]
+	# todo: define the pattern that matches bash variables once
+	#       and use that pattern var instead hardcodet patterns
+	if [[ "$val" =~ ^\"\$([a-zA-Z_][a-zA-Z0-9_])\"$ ]]
 	then
 		val="${BASH_REMATCH[1]}"
 	# unquoted var
@@ -113,9 +185,14 @@ function bash_value_to_str_slice() {
 	then
 		val="\"$val\""
 	# quoted string
-	elif [[ "$val" =~ ^\".*\" ]]
+	elif [[ "$val" =~ ^\".*\"$ ]]
 	then
-		test
+		local oldval="$val"
+		interpolate_to_fmt "$val" >&2
+		val="$(interpolate_to_fmt "$val")"
+		# no interpolation found
+		# fallback to quoted str slice
+		[[ "$val" == "" ]] && val="$oldval"
 	# unquoted string
 	elif [[ "$val" =~ ^([^ ]+) ]]
 	then
---
+++ test/interpolate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+foo=bar
+
+echo "foo $foo"
+
+world="cruel world"
+greet="hello $world"
+
+echo "$greet"
+
+echo "$greet and $foo"
+
